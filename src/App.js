89ed64039bf27972601028858a9db38fe4875f58
yerbol kosbayev <yerbolkosbayev@gmail.com>
2019-11-11 22:10:48 +0600

    created Users and UserItem components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar";
+import Users from "./components/users/Users";
 import "./App.css";
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div className='App'>
         <Navbar />
+        <Users />
       </div>
     );
   }
