ee8b45282ce24be3a9778bc8e7423a62a0b4d792
yerbol kosbayev <yerbolkosbayev@gmail.com>
2019-11-14 16:04:26 +0600

    refactored whole app state management using context

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -35,11 +35,35 @@ const GithubState = props => {
   };
 
   // Get User
+  const getUser = async username => {
+    setLoading();
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    dispatch({
+      type: GET_USER,
+      payload: res.data
+    });
+  };
 
   // Get Repos
+  const getUserRepos = async username => {
+    setLoading();
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    dispatch({
+      type: GET_REPOS,
+      payload: res.data
+    });
+  };
 
   // Clear User
-
+  const clearUsers = () => dispatch({ type: CLEAR_USERS });
   // Set Loading
   const setLoading = () => dispatch({ type: SET_LOADING });
 
@@ -50,7 +74,10 @@ const GithubState = props => {
         user: state.user,
         repos: state.repos,
         loading: state.loading,
-        searchUsers
+        searchUsers,
+        clearUsers,
+        getUser,
+        getUserRepos
       }}
     >
       {props.children}
